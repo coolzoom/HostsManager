1d014d5131f3f76976f5340395724d645c3b2fa9
wangcz <wangcz@2345.com>
2018-07-31 08:35:43 +0800

    QSID: 产品人员: 代码检查: 简述:

---
+++ HostsManager/Form1.Designer.cs
@@ -0,0 +1,160 @@
+﻿namespace HostsManager
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.lbHosts = new System.Windows.Forms.ListBox();
+            this.lbIPS = new System.Windows.Forms.ListBox();
+            this.btnLoadHosts = new System.Windows.Forms.Button();
+            this.tbHosts = new System.Windows.Forms.TextBox();
+            this.btnAddHosts = new System.Windows.Forms.Button();
+            this.btnOpenHosts = new System.Windows.Forms.Button();
+            this.notifyIcon1 = new System.Windows.Forms.NotifyIcon(this.components);
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.SuspendLayout();
+            // 
+            // lbHosts
+            // 
+            this.lbHosts.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left)));
+            this.lbHosts.BackColor = System.Drawing.SystemColors.Window;
+            this.lbHosts.Font = new System.Drawing.Font("宋体", 11F);
+            this.lbHosts.FormattingEnabled = true;
+            this.lbHosts.ItemHeight = 15;
+            this.lbHosts.Location = new System.Drawing.Point(0, 30);
+            this.lbHosts.Margin = new System.Windows.Forms.Padding(0);
+            this.lbHosts.Name = "lbHosts";
+            this.lbHosts.Size = new System.Drawing.Size(300, 424);
+            this.lbHosts.TabIndex = 0;
+            this.lbHosts.SelectedIndexChanged += new System.EventHandler(this.lbHosts_SelectedIndexChanged);
+            // 
+            // lbIPS
+            // 
+            this.lbIPS.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.lbIPS.Font = new System.Drawing.Font("宋体", 11F);
+            this.lbIPS.FormattingEnabled = true;
+            this.lbIPS.ItemHeight = 15;
+            this.lbIPS.Location = new System.Drawing.Point(300, 30);
+            this.lbIPS.Margin = new System.Windows.Forms.Padding(0);
+            this.lbIPS.Name = "lbIPS";
+            this.lbIPS.Size = new System.Drawing.Size(506, 424);
+            this.lbIPS.TabIndex = 3;
+            this.lbIPS.SelectedIndexChanged += new System.EventHandler(this.lbIPS_SelectedIndexChanged);
+            // 
+            // btnLoadHosts
+            // 
+            this.btnLoadHosts.Location = new System.Drawing.Point(0, 2);
+            this.btnLoadHosts.Name = "btnLoadHosts";
+            this.btnLoadHosts.Size = new System.Drawing.Size(75, 23);
+            this.btnLoadHosts.TabIndex = 4;
+            this.btnLoadHosts.Text = "加载Hosts";
+            this.btnLoadHosts.UseVisualStyleBackColor = true;
+            this.btnLoadHosts.Click += new System.EventHandler(this.btnLoadHosts_Click);
+            // 
+            // tbHosts
+            // 
+            this.tbHosts.Location = new System.Drawing.Point(181, 4);
+            this.tbHosts.Name = "tbHosts";
+            this.tbHosts.Size = new System.Drawing.Size(286, 21);
+            this.tbHosts.TabIndex = 5;
+            this.tbHosts.Text = "127.0.0.1 e.2345.com";
+            // 
+            // btnAddHosts
+            // 
+            this.btnAddHosts.Location = new System.Drawing.Point(473, 4);
+            this.btnAddHosts.Name = "btnAddHosts";
+            this.btnAddHosts.Size = new System.Drawing.Size(75, 23);
+            this.btnAddHosts.TabIndex = 6;
+            this.btnAddHosts.Text = "添加绑定";
+            this.btnAddHosts.UseVisualStyleBackColor = true;
+            this.btnAddHosts.Click += new System.EventHandler(this.btnAddHosts_Click);
+            // 
+            // btnOpenHosts
+            // 
+            this.btnOpenHosts.Location = new System.Drawing.Point(81, 2);
+            this.btnOpenHosts.Name = "btnOpenHosts";
+            this.btnOpenHosts.Size = new System.Drawing.Size(75, 23);
+            this.btnOpenHosts.TabIndex = 7;
+            this.btnOpenHosts.Text = "打开Hosts";
+            this.btnOpenHosts.UseVisualStyleBackColor = true;
+            this.btnOpenHosts.Click += new System.EventHandler(this.btnOpenHosts_Click);
+            // 
+            // notifyIcon1
+            // 
+            this.notifyIcon1.ContextMenuStrip = this.contextMenuStrip1;
+            this.notifyIcon1.Icon = ((System.Drawing.Icon)(resources.GetObject("notifyIcon1.Icon")));
+            this.notifyIcon1.Text = "HostsManager";
+            this.notifyIcon1.Visible = true;
+            this.notifyIcon1.DoubleClick += new System.EventHandler(this.notifyIcon1_DoubleClick);
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(61, 4);
+            this.contextMenuStrip1.ItemClicked += new System.Windows.Forms.ToolStripItemClickedEventHandler(this.contextMenuStrip1_ItemClicked);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(806, 471);
+            this.Controls.Add(this.btnOpenHosts);
+            this.Controls.Add(this.btnAddHosts);
+            this.Controls.Add(this.tbHosts);
+            this.Controls.Add(this.btnLoadHosts);
+            this.Controls.Add(this.lbIPS);
+            this.Controls.Add(this.lbHosts);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "HostsManager";
+            this.TopMost = true;
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.Resize += new System.EventHandler(this.Form1_Resize);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox lbHosts;
+        private System.Windows.Forms.ListBox lbIPS;
+        private System.Windows.Forms.Button btnLoadHosts;
+        private System.Windows.Forms.TextBox tbHosts;
+        private System.Windows.Forms.Button btnAddHosts;
+        private System.Windows.Forms.Button btnOpenHosts;
+        private System.Windows.Forms.NotifyIcon notifyIcon1;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+    }
+}
+
---
+++ HostsManager/Form1.cs
@@ -0,0 +1,472 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace HostsManager
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+            //this.WindowState = FormWindowState.Minimized;
+            //this.Hide();
+        }
+
+        private const string hostsFilePath = @"C:\Windows\System32\drivers\etc\hosts";
+        private static Dictionary<string,List<string>> hostsDic;
+        private static Dictionary<string, string> hosts2IP;
+        private const string localhost = "localhost";
+        private const string disabledIP = "不指定IP地址";
+        private static string lineBreak = "\r\n";
+        private static bool isNeedSwitch = false;
+        private static bool isClose = false;
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            lbHosts.Items.Clear();
+            hostsDic = new Dictionary<string, List<string>>();
+            hosts2IP = new Dictionary<string, string>();
+            // 1-读取hosts文件
+            string hostsContent = this.Read(Form1.hostsFilePath);
+            //label1.Text = hostsContent;
+            
+            foreach (KeyValuePair<string,List<string>> item in hostsDic)
+            {
+                // 2-加载主界面域名列表
+                lbHosts.Items.Add(item.Key);
+                
+                // 3-加载通知栏菜单
+                ToolStripMenuItem itemTool = (ToolStripMenuItem)contextMenuStrip1.Items.Add(item.Key);
+                ToolStripMenuItem itemDropDisabledIP = (ToolStripMenuItem)itemTool.DropDownItems.Add(disabledIP);
+                itemDropDisabledIP.ToolTipText = item.Key;
+                itemDropDisabledIP.Click += new EventHandler(tsmiClick1);//绑定方法
+                itemDropDisabledIP.ForeColor = Color.Red;
+
+                string ipSetted = hosts2IP.ContainsKey(item.Key) ? hosts2IP[item.Key] : "";
+                foreach (string ip in item.Value)
+                {
+                    ToolStripMenuItem itemDrop = (ToolStripMenuItem)itemTool.DropDownItems.Add(ip);
+                    itemDrop.ToolTipText = item.Key;
+                    if(ip == ipSetted)
+                    {
+                        itemDrop.ForeColor = Color.Red;
+                        itemDropDisabledIP.ForeColor = Color.Black;
+                    }
+                    itemDrop.Click += new EventHandler(tsmiClick1);//绑定方法
+                }
+            }
+
+            // 4-增加退出功能
+            contextMenuStrip1.Items.Add("退出");
+        }
+
+        private void tsmiClick1(object sender, EventArgs e)
+        {
+            ToolStripMenuItem itemDrop = (ToolStripMenuItem)sender;
+            ToolStrip parentStrip = itemDrop.GetCurrentParent();
+            foreach(ToolStripItem item in parentStrip.Items)
+            {
+                item.ForeColor = Color.Black;
+            }
+            itemDrop.ForeColor = Color.Red;
+            //MessageBox.Show(itemDrop.ToolTipText + itemDrop.Text);
+            setHostIP(itemDrop.ToolTipText, itemDrop.Text);
+        }
+
+        private string Read(string path)
+        {
+            hosts2IP = new Dictionary<string, string>();
+            StreamReader sr = new StreamReader(path, Encoding.Default);
+            String line;
+            string content = "";
+            string lineString = "";
+            List<string> ipList = new List<string>();
+            while ((line = sr.ReadLine()) != null)
+            {
+                lineString  = line.ToString().Replace("#", "").Trim();
+                //lbHosts.Items.Add(line.ToString());
+                // trim之后，第一个字符不是数据则跳过
+                if (lineString.Length>0 && this.isNumberic(lineString.Substring(0,1)))
+                {
+                    List<string> lineDomain = MatchsDomain(lineString);
+                    // 兼容localhost
+                    if (lineDomain.Count == 1 && lineString.IndexOf(localhost) >0)
+                    {
+                        genHostsDic(lineDomain[0], localhost);
+                        //lbHosts.Items.Add(lineDomain[0]);
+                        //lbHosts.Items.Add(localhost);
+
+                        if (line.ToString().IndexOf("#") < 0)
+                        {
+                            //设置每个域名对应的IP地址，判断是否含有字符#
+                            if (hosts2IP.ContainsKey(localhost))
+                            {
+                                hosts2IP[localhost] = lineDomain[0];
+                            }
+                            else
+                            {
+                                hosts2IP.Add(localhost, lineDomain[0]);
+                            }
+                        }
+                    }
+                    // 格式化出域名和IP
+                    // 添加到hosts列表中
+                    if (lineDomain.Count == 2)
+                    {
+                        genHostsDic(lineDomain[0], lineDomain[1]);
+                        //lbHosts.Items.Add(lineDomain[0]);
+                        //lbHosts.Items.Add(lineDomain[1]);
+
+                        if (line.ToString().IndexOf("#") < 0)
+                        {
+                            //设置每个域名对应的IP地址，判断是否含有字符#
+                            if (hosts2IP.ContainsKey(lineDomain[1]))
+                            {
+                                hosts2IP[lineDomain[1]] = lineDomain[0];
+                            }
+                            else
+                            {
+                                hosts2IP.Add(lineDomain[1], lineDomain[0]);
+                            }
+                        }
+                    }
+                }
+            }
+            sr.Close();
+            return content;
+        }
+
+        private static void genHostsDic(string ip,string domain)
+        {
+            List<string> ipList = new List<string>();
+            if (hostsDic.ContainsKey(domain))
+            {
+                ipList = hostsDic[domain];
+                ipList.Add(ip);
+                hostsDic[domain] = ipList;
+            }
+            else
+            {
+                ipList.Add(ip);
+                hostsDic[domain] = ipList;
+            }
+        }
+
+        private void Write(string path,string content, System.Text.Encoding encoding)
+        {
+            File.WriteAllText(path, content);
+            //FileStream fs = new FileStream(path, FileMode.Open, FileAccess.Read, FileShare.ReadWrite);
+            //StreamWriter sw = new StreamWriter(fs, Encoding.Default);
+
+            //sw.Write(content);
+            //sw.Close();
+            //fs.Close();
+        }
+
+        private bool isNumberic(string message)
+        {
+            System.Text.RegularExpressions.Regex rex =
+            new System.Text.RegularExpressions.Regex(@"^\d+$");
+            int result = -1;
+            if (rex.IsMatch(message))
+            {
+                result = int.Parse(message);
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        /// <summary>  
+        /// 匹配获取字符串中所有的域名  
+        /// </summary>  
+        /// <param name="input"></param>  
+        /// <returns></returns>  
+        public static List<string> MatchsDomain(string input)
+        {
+            string pattern = @"[a-zA-Z0-9][-a-zA-Z0-9]{0,62}(\.[a-zA-Z0-9][-a-zA-Z0-9]{0,62})+";
+            return Matchs(input, pattern);
+        }
+        /// <summary>  
+        /// 匹配结果  返回匹配结果的数组  
+        /// </summary>  
+        /// <param name="input"></param>  
+        /// <param name="expression"></param>  
+        /// <returns></returns>  
+        public static List<string> Matchs(string input, string expression)
+        {
+            List<string> list = new List<string>();
+            MatchCollection collection = Regex.Matches(input, expression, RegexOptions.IgnoreCase | RegexOptions.Multiline);
+            foreach (Match item in collection)
+            {
+                if (item.Success)
+                {
+                    list.Add(item.Value);
+                }
+            }
+            return list;
+        }
+
+        private void lbHosts_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            isNeedSwitch = false;
+            if(lbHosts.SelectedIndex<0)
+            {
+                return;
+            }
+            string domain = lbHosts.SelectedItem.ToString();
+            if(domain.Trim().Length<=0)
+            {
+                return;
+            }
+            if(!hostsDic.ContainsKey(domain))
+            {
+                return;
+            }
+            lbIPS.Items.Clear();
+            int index = 0;
+            lbIPS.Items.Add(disabledIP);
+            lbIPS.SelectedIndex = index;
+            List<string> ipList = hostsDic[domain];
+            string ipSetted = hosts2IP.ContainsKey(domain) ? hosts2IP[domain] : "";
+            foreach(string ip in ipList)
+            {
+                lbIPS.Items.Add(ip);
+                index++;
+                if (ipSetted == ip)
+                {
+                    lbIPS.SelectedIndex = index;
+                }
+            }
+            // 设置选中
+            isNeedSwitch = true;
+        }
+
+        private void lbIPS_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if(isNeedSwitch == false)
+            {
+                return;
+            }
+            string domain = lbHosts.SelectedItem.ToString();
+            if(domain.Length<=0)
+            {
+                return;
+            }
+            setHostIP(domain, lbIPS.SelectedItem.ToString());
+        }
+
+        private void setHostIP(string domain, string ip)
+        {
+            if (domain.Length <= 0)
+            {
+                return;
+            }
+            bool isSetted = false;
+
+            StreamReader sr = new StreamReader(hostsFilePath, Encoding.Default);
+            String line;
+            string content = "";
+            string lineString = "";
+            List<string> ipList = new List<string>();
+            while ((line = sr.ReadLine()) != null)
+            {
+                lineString = line.ToString().Replace("#", "").Trim();
+                //lbHosts.Items.Add(line.ToString());
+                // trim之后，第一个字符不是数据则跳过
+                if (lineString.Length > 0 && this.isNumberic(lineString.Substring(0, 1)))
+                {
+                    List<string> lineDomain = MatchsDomain(lineString);
+                    // 兼容localhost
+                    if (lineDomain.Count == 1 && lineString.IndexOf(localhost) > 0)
+                    {
+                        if (domain == localhost)
+                        {
+                            if (lineDomain[0] == ip)
+                            {
+                                content += ip + " " + domain + lineBreak;
+                            }
+                            else
+                            {
+                                content += "#" + lineDomain[0] + " " + domain + lineBreak;
+                            }
+                            isSetted = true;
+                            continue;
+                        }
+                    }
+                    // 格式化出域名和IP
+                    // 添加到hosts列表中
+                    if (lineDomain.Count == 2)
+                    {
+                        //genHostsDic(lineDomain[0], lineDomain[1]);
+                        //lbHosts.Items.Add(lineDomain[0]);
+                        //lbHosts.Items.Add(lineDomain[1]);
+                        if (domain == lineDomain[1])
+                        {
+                            if (lineDomain[0] == ip)
+                            {
+                                content += ip + " " + domain + lineBreak;
+                            }
+                            else
+                            {
+                                content += "#" + lineDomain[0] + " " + domain + lineBreak;
+                            }
+                            isSetted = true;
+                            continue;
+                        }
+                    }
+                }
+                content += line.ToString() + lineBreak;
+            }
+            if (!isSetted)
+            {
+                content += ip + " " + domain + lineBreak;
+            }
+            sr.Close();
+            Write(hostsFilePath, content, Encoding.Default);
+        }
+
+        private void btnLoadHosts_Click(object sender, EventArgs e)
+        {
+            hostsDic = new Dictionary<string, List<string>>();
+            // 1-读取hosts文件
+            string hostsContent = this.Read(Form1.hostsFilePath);
+            //label1.Text = hostsContent;
+
+            lbHosts.Items.Clear();
+            foreach (KeyValuePair<string, List<string>> item in hostsDic)
+            {
+                lbHosts.Items.Add(item.Key);
+            }
+        }
+
+        private void btnAddHosts_Click(object sender, EventArgs e)
+        {
+            string ipHosts = tbHosts.Text.ToString().Replace("#", "").Trim();
+            List<string> lineDomain = MatchsDomain(ipHosts);
+            // 判断数据是否合法，格式是否正确
+
+            string domain = lineDomain[1];
+            if (domain.Length <= 0)
+            {
+                return;
+            }
+            string ip = lineDomain[0];
+            bool isSetted = false;
+
+            StreamReader sr = new StreamReader(hostsFilePath, Encoding.Default);
+            String line;
+            string content = "";
+            string lineString = "";
+            List<string> ipList = new List<string>();
+            while ((line = sr.ReadLine()) != null)
+            {
+                lineString = line.ToString().Replace("#", "").Trim();
+                //lbHosts.Items.Add(line.ToString());
+                // trim之后，第一个字符不是数据则跳过
+                if (lineString.Length > 0 && this.isNumberic(lineString.Substring(0, 1)))
+                {
+                    lineDomain = MatchsDomain(lineString);
+                    // 兼容localhost
+                    if (lineDomain.Count == 1 && lineString.IndexOf(localhost) > 0)
+                    {
+                        if (domain == localhost && lineDomain[0] == ip)
+                        {
+                            isSetted = true;
+                        }
+                    }
+                    // 格式化出域名和IP
+                    // 添加到hosts列表中
+                    if (lineDomain.Count == 2)
+                    {
+                        //genHostsDic(lineDomain[0], lineDomain[1]);
+                        //lbHosts.Items.Add(lineDomain[0]);
+                        //lbHosts.Items.Add(lineDomain[1]);
+                        if (domain == lineDomain[1] && lineDomain[0] == ip)
+                        {
+                            isSetted = true;
+                        }
+                    }
+                }
+                content += line.ToString() + lineBreak;
+            }
+            if (!isSetted)
+            {
+                content += ip + " " + domain + lineBreak;
+            }
+            sr.Close();
+            Write(hostsFilePath, content, Encoding.Default);
+
+            // 重新加载Hosts
+            hostsDic = new Dictionary<string, List<string>>();
+            // 1-读取hosts文件
+            string hostsContent = this.Read(Form1.hostsFilePath);
+            //label1.Text = hostsContent;
+
+            lbHosts.Items.Clear();
+            foreach (KeyValuePair<string, List<string>> item in hostsDic)
+            {
+                lbHosts.Items.Add(item.Key);
+            }
+        }
+
+        private void btnOpenHosts_Click(object sender, EventArgs e)
+        {
+            // 打开文件
+            System.Diagnostics.Process.Start("notepad.exe", hostsFilePath);
+        }
+
+        private void contextMenuStrip1_ItemClicked(object sender, ToolStripItemClickedEventArgs e)
+        {            
+            if (e.ClickedItem.Text == "退出")
+            {
+                isClose = true;
+                this.Close();
+            }
+        }
+
+        private void Form1_Resize(object sender, EventArgs e)
+        {
+            if (this.WindowState == FormWindowState.Minimized)
+            {
+                this.Hide();
+                this.ShowInTaskbar = false;
+                this.notifyIcon1.Visible = true;
+            }
+        }
+
+        private void notifyIcon1_DoubleClick(object sender, EventArgs e)
+        {
+            this.Show();
+            if(this.WindowState != FormWindowState.Normal)
+            {
+                this.WindowState = FormWindowState.Normal;
+            }
+            //this.notifyIcon1.Visible = false;
+            //this.ShowInTaskbar = true;
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            if(!isClose)
+            {
+                this.Hide();
+                this.ShowInTaskbar = false;
+                this.notifyIcon1.Visible = true;
+                e.Cancel = true;
+            }
+        }
+    }
+}
